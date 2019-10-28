4d9eda3838ef0ceb780b3e5097b010ad3d05f1f8
Theodoros Konstantoulias <thodoriskonstantoulias@yahoo.com>
2019-10-28 22:59:11 +0200

    JQuery testing

--- DutchTreat/wwwroot/js/index.js
+++ DutchTreat/wwwroot/js/index.js
@@ -1,7 +1,23 @@
-﻿var form = document.getElementById("theForm");
-console.log("Starting...");
-var button = document.getElementById("theButton");
-button.addEventListener("click", function () {
-	
-	alert("Button pressed!!!");
-})
+﻿$(document).ready(function () {
+
+
+	var form = $("#theForm");
+	//form.hide();
+	console.log("Starting...");
+
+
+
+	var button = $("#theButton");
+	button.on("click", function () {
+
+		alert("Button pressed!!!");
+	})
+
+
+	$loginToggle = $("#loginToggle");
+	$popupForm = $(".popupForm");
+
+	$loginToggle.on("click", function () {
+		$popupForm.toggle(1000);
+	});
+});
