2ff1d2403cf2fcf0bea90a639aee986a7aae19f4
Theodoros Konstantoulias <thodoriskonstantoulias@yahoo.com>
2019-10-29 23:00:27 +0200

    Added MVC, controllers, views etc

---
+++ DutchTreat/Controllers/AppController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DutchTreat.Controllers
+{
+    public class AppController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Contact()
+        {
+            ViewBag.Title = "Contact Us";
+            return View();
+        }
+        public IActionResult About()
+        {
+            ViewBag.Title = "About Us";
+            return View();
+        }
+    }
+}
--- DutchTreat/Startup.cs
+++ DutchTreat/Startup.cs
@@ -16,14 +16,26 @@ namespace DutchTreat
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddControllersWithViews();
+            services.AddRazorPages();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            app.UseDefaultFiles();
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            
             app.UseStaticFiles();
             app.UseNodeModules();
+            app.UseRouting();
+            app.UseEndpoints(cfg =>
+            {
+                cfg.MapControllerRoute(name: "default", pattern:"{controller=App}/{action=Index}/{id?}");
+                cfg.MapRazorPages();
+            }); 
         }
     }
 }
