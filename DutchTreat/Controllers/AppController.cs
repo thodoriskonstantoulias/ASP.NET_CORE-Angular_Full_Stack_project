0cc9da4c82779174d0da61b8eab0f326380cb0a3
Theodoros Konstantoulias <thodoriskonstantoulias@yahoo.com>
2019-10-30 23:15:31 +0200

    Added client side validation , added service for fake mail

--- DutchTreat/Controllers/AppController.cs
+++ DutchTreat/Controllers/AppController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using DutchTreat.Services;
 using DutchTreat.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 
@@ -9,6 +10,12 @@ namespace DutchTreat.Controllers
 {
     public class AppController : Controller
     {
+        private readonly INullMailService mailService;
+
+        public AppController(INullMailService mailService)
+        {
+            this.mailService = mailService;
+        }
         public IActionResult Index()
         {
             return View();
@@ -22,6 +29,17 @@ namespace DutchTreat.Controllers
         [HttpPost("contact")]
         public IActionResult Contact(ContactViewModel model)
         {
+            if (ModelState.IsValid)
+            {
+                mailService.SendMessage("xxx@gmail.com", model.Subject, model.Message);
+                ViewBag.UserMessage = "Mail sent";
+                ModelState.Clear();
+            }
+            else
+            {
+
+            }
+
             return View();
         }
         public IActionResult About()
