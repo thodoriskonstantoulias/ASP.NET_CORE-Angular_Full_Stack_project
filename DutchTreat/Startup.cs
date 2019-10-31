ba7ddba8940e6aac5b78d39613b84c9b66042174
Theodoros Konstantoulias <thodoriskonstantoulias@yahoo.com>
2019-10-31 23:00:40 +0200

    Add EF Core basis ex. context class and models

--- DutchTreat/Startup.cs
+++ DutchTreat/Startup.cs
@@ -2,10 +2,12 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using DutchTreat.Data;
 using DutchTreat.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
@@ -17,6 +19,9 @@ namespace DutchTreat
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<DutchContext>(cfg => {
+                cfg.UseSqlServer("");
+            });
             services.AddTransient<INullMailService, NullMailService>();
             services.AddControllersWithViews().AddRazorRuntimeCompilation(); 
             services.AddRazorPages();
