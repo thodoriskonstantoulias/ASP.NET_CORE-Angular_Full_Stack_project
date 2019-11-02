dd5cb30770f502b853fb9b7fca263dd7d019fb24
Theodoros Konstantoulias <thodoriskonstantoulias@yahoo.com>
2019-11-02 15:56:25 +0200

    Created API controller, GET

---
+++ DutchTreat/Controllers/ProductsController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DutchTreat.Data;
+using DutchTreat.Data.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace DutchTreat.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private readonly IDutchRepository context;
+        private readonly ILogger<ProductsController> logger;
+
+        public ProductsController(IDutchRepository context, ILogger<ProductsController> logger)
+        {
+            this.context = context;
+            this.logger = logger;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            try
+            {
+                return Ok(context.GetAllProducts());
+            }
+            catch (Exception ex)
+            {
+                logger.LogError($"Failed to get products{ex}");
+                return BadRequest("Failed to get products");               
+            }
+            
+        }
+    }
+}
