ca8ffa05954f7341bb1be60b1e145557855ffe76
Theodoros Konstantoulias <thodoriskonstantoulias@yahoo.com>
2019-11-03 13:38:40 +0200

    Created Login View

---
+++ DutchTreat/Controllers/AccountController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace DutchTreat.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly ILogger<AccountController> logger;
+
+        public AccountController(ILogger<AccountController> logger)
+        {
+            this.logger = logger;
+        }
+        public IActionResult Login()
+        {
+            if (this.User.Identity.IsAuthenticated)
+            {
+                return RedirectToAction("Index", "App");
+            }
+            return View();
+        }
+    }
+}
