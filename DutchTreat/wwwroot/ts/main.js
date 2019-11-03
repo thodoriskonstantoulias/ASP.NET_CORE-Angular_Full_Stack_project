f58152d4cc149cfd775dc7beb1e44435adbba585
Theodoros Konstantoulias <thodoriskonstantoulias@yahoo.com>
2019-11-03 22:44:29 +0200

    Typescript basics

---
+++ DutchTreat/wwwroot/ts/main.js
@@ -0,0 +1,3 @@
+var shopper = new StoreCustomer("Ted", "Kos");
+shopper.showName();
+//# sourceMappingURL=main.js.map
---
+++ DutchTreat/wwwroot/ts/main.ts
@@ -0,0 +1,3 @@
+﻿
+let shopper = new StoreCustomer("Ted", "Kos");
+shopper.showName();
